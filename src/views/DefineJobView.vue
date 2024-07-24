39f699f3f6c80c1bd4ced9b82b17f854aa2873c3
Ghassen AMAIRI <ghassen.amairi@teamwillgroup.com>
2024-07-24 13:59:47 +0100

    resolving merge conflicts

---
+++ src/views/DefineJobView.vue
@@ -0,0 +1,15 @@
+<template>
+        <h1 class="text-center">Define your ideal job</h1>
+
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+
+
+export default class DefineJob extends Vue{
+ 
+
+}
+</script>
+
